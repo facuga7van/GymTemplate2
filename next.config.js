0b2b119103c2d3f9f6bf1da10f1a641e35b1b9f6
facuga7van <70443572+facuga7van@users.noreply.github.com>
2024-09-01 03:19:35 -0300

    preparing deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: '/GymTemplat2',
+    assetPrefix: '/GymTemplate2/',
 }
 
 module.exports = nextConfig
