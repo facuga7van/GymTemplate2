1050df76d2fbe3a9c9314c68b7d0e62b2771ebf6
facuga7van <70443572+facuga7van@users.noreply.github.com>
2024-09-01 04:02:07 -0300

    no entiendo

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const isProd = process.env.NODE_ENV = 'production';
 const nextConfig = {
     output: 'export',
     basePath: isProd ? '/GymTemplate2' : '',
-    assetPrefix: isProd ? '/GymTemplate2/' : '',
+    assetPrefix: isProd ? '/GymTemplate2/' : ''
 }
 
 module.exports = nextConfig
